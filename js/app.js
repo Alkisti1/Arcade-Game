cfd8ce5435a70665b344d99cf9e1ddc931505149
Alkisti Douka <ad537@cornell.edu>
2018-05-01 19:13:17 +0300

    Basic Game Works

--- js/app.js
+++ js/app.js
@@ -24,18 +24,18 @@ Enemy.prototype.update = function(dt) {
     {
       this.x=this.x +(dt*this.speed);
       // Reposition bugs and randomize speed
-      if(this.x>510) {
-        this.x=-50;
-        this.speed=100 + Math.floor(Math.random()*222);
-      }
-//reset player to the beginning
-      if (player.x<this.x +80 &&
-        player.x +80 > this.x &&
-        player.y<this.yy +60 &&
-        60 + player.y>this.y) {
-          player.x=202;
-          player.y=405;
+      if(this.x>550) {
+        this.x=-100;
+        this.speed=100 + Math.floor(Math.random()*512);
         }
+// Check for collisions enemy vs Player
+if (player.x < this.x + 50 &&
+    player.x + 37 > this.x &&
+    player.y < this.y + 25 &&
+    30 + player.y > this.y) {
+    player.x = 200;
+    player.y = 380;
+}
 
     }
 };
@@ -54,32 +54,69 @@ class Player {
 }
 // This class requires an update(),
 update() {
-
+ if(this.y < 0) {
+   this.x = 200;
+   this.y =380;
+ }
 }
 //render() and
 render() {
   ctx.drawImage(Resources.get(this.player), this.x, this.y);
 };
 // a handleInput() method.
-handleInput(){
-
+handleInput(keyInput) {
+  switch (keyInput) {
+          case "left":
+              //check for wall, otherwise move left
+              if (this.x > 0) {
+                  this.x -= 101;
+              }
+              break;
+          case "right":
+              //check for wall, otherwise move right
+              if (this.x < 402) {
+                  this.x += 101;
+              }
+              break;
+          case "up":
+              //check if player reached top of water, if so call success function,
+              // otherwise move up
+              if (this.y < 0) {
+                  this.x = 200;
+                  this.y=380;
+              } else {
+                  this.y -= 83;
+              }
+              break;
+          case "down":
+              //check for bottom, otherwise move down
+              if (this.y < 400) {
+                  this.y += 83;
+              }
+              break;
+      }
 }
 };
 
-var player= new Player (0,200,400)
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-const firstEnemy = new Enemy(-100, 60);
-const secondEnemy = new Enemy(-100, 145);
-const thirdEnemy = new Enemy(-100, 230);
-var allEnemies=[firstEnemy, secondEnemy, thirdEnemy];
+var allEnemies=[];
+var enemyPosition = [60,140,220];
+
+enemyPosition.forEach(function(posY) {
+  enemy= new Enemy(0, posY, 100 + Math.floor(Math.random()*512));
+  allEnemies.push(enemy);
+});
 // Place the player object in a variable called player
+var player= new Player (200,400);
+
+
 
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+var input=function(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
@@ -88,4 +125,5 @@ document.addEventListener('keyup', function(e) {
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
-});
+};
+ document.addEventListener('keyup', input);
